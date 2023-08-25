9138f5ee4ac8007ba8dc80fda9969993c453c966
houssemgnounou <47723455+houssemgnounou@users.noreply.github.com>
2023-08-25 12:45:33 +0100

    Crud

--- eleve.cpp
+++ eleve.cpp
@@ -4,6 +4,124 @@
 
 Eleve::Eleve()
 {
-
+    eleveId  = 0;
+    nom = "";
+    prenom = "";
+    dateNaissance = QDate();
+    adresse = "";
+    nomParentTuteur = "";
+    numeroTelephoneParent = "";
+    dateInscription = QDate();
+    classe = 0;
 }
 
+Eleve::Eleve(int id, QString nom, QString prenom, QDate dateNaissance, QString adresse, QString nomParentTuteur,
+             QString numeroTelephoneParent, QDate dateInscription, int classe)
+{
+    this->eleveId  = id;
+    this->nom = nom;
+    this->prenom = prenom;
+    this->dateNaissance = dateNaissance;
+    this->adresse = adresse;
+    this->nomParentTuteur = nomParentTuteur;
+    this->numeroTelephoneParent = numeroTelephoneParent;
+    this->dateInscription = dateInscription;
+    this->classe = classe;
+}
+
+QString Eleve::getNom()
+{
+    return nom;
+}
+
+QString Eleve::getPrenom()
+{
+    return prenom;
+}
+
+
+bool Eleve::ajouter()
+{
+    QSqlQuery query;
+    query.prepare("INSERT INTO Eleves (Nom, Prenom, Date_Naissance, Adresse, Nom_Parent_Tuteur, Numero_Telephone_Parent, Date_Inscription, Classe) "
+                  "VALUES (:nom, :prenom, :dateNaissance, :adresse, :nomParentTuteur, :numeroTelephoneParent, :dateInscription, :classe)");
+    query.bindValue(":nom", nom);
+    query.bindValue(":prenom", prenom);
+    query.bindValue(":dateNaissance", dateNaissance);
+    query.bindValue(":adresse", adresse);
+    query.bindValue(":nomParentTuteur", nomParentTuteur);
+    query.bindValue(":numeroTelephoneParent", numeroTelephoneParent);
+    query.bindValue(":dateInscription", dateInscription);
+    query.bindValue(":classe", classe);
+
+    if (query.exec()) {
+        return true;
+    } else {
+        qDebug() << "Error in ajouter(): " << query.lastError().text();
+        return false;
+    }
+}
+
+bool Eleve::modifier()
+{
+    QSqlQuery query;
+    query.prepare("UPDATE Eleves SET Nom = :nom, Prenom = :prenom, Date_Naissance = :dateNaissance, "
+                  "Adresse = :adresse, Nom_Parent_Tuteur = :nomParentTuteur, Numero_Telephone_Parent = :numeroTelephoneParent, "
+                  "Date_Inscription = :dateInscription, Classe = :classe WHERE ID_Eleve = :eleveId");
+    query.bindValue(":nom", nom);
+    query.bindValue(":prenom", prenom);
+    query.bindValue(":dateNaissance", dateNaissance);
+    query.bindValue(":adresse", adresse);
+    query.bindValue(":nomParentTuteur", nomParentTuteur);
+    query.bindValue(":numeroTelephoneParent", numeroTelephoneParent);
+    query.bindValue(":dateInscription", dateInscription);
+    query.bindValue(":classe", classe);
+    query.bindValue(":eleveId", eleveId);
+
+    if (query.exec()) {
+        return true;
+    } else {
+        qDebug() << "Error in modifier(): " << query.lastError().text();
+        return false;
+    }
+}
+
+bool Eleve::supprimer()
+{
+    QSqlQuery query;
+    query.prepare("DELETE FROM Eleves WHERE ID_Eleve = :eleveId");
+    query.bindValue(":eleveId", eleveId);
+
+    if (query.exec()) {
+        return true;
+    } else {
+        qDebug() << "Error in supprimer(): " << query.lastError().text();
+        return false;
+    }
+}
+
+QSqlQueryModel* Eleve::rechercher(QString nom, QString prenom, int classe)
+{
+    QSqlQueryModel* model = new QSqlQueryModel();
+    QSqlQuery query;
+
+    query.prepare("SELECT * FROM Eleves WHERE Nom LIKE :nom AND Prenom LIKE :prenom AND Classe = :classe");
+    query.bindValue(":nom", "%" + nom + "%");
+    query.bindValue(":prenom", "%" + prenom + "%");
+    query.bindValue(":classe", classe);
+
+    if (query.exec()) {
+        model->setQuery(query);
+    } else {
+        qDebug() << "Error in rechercher(): " << query.lastError().text();
+    }
+
+    return model;
+}
+
+QSqlQueryModel* Eleve::trierParNomDateInscriptionClasse()
+{
+    QSqlQueryModel* model = new QSqlQueryModel();
+    model->setQuery("SELECT * FROM Eleves ORDER BY Nom ASC, Date_Inscription ASC, Classe ASC");
+    return model;
+}
--- enseignant.cpp
+++ enseignant.cpp
@@ -4,6 +4,128 @@
 
 Enseignant::Enseignant()
 {
-
+    enseignantId =0;
+    nom = "";
+    prenom = "";
+    dateNaissance = QDate();
+    adresse = "";
+    email = "";
+    numeroTelephone = "";
+    dateEmbauche = QDate();
+    matiereEnseignee = "";
 }
 
+
+
+Enseignant::Enseignant(int id, QString nom, QString prenom, QDate dateNaissance, QString adresse, QString email,
+                       QString numeroTelephone, QDate dateEmbauche, QString matiereEnseignee)
+{
+    this->enseignantId  = id;
+    this->nom = nom;
+    this->prenom = prenom;
+    this->dateNaissance = dateNaissance;
+    this->adresse = adresse;
+    this->email = email;
+    this->numeroTelephone = numeroTelephone;
+    this->dateEmbauche = dateEmbauche;
+    this->matiereEnseignee = matiereEnseignee;
+}
+
+
+QString Enseignant::getNom()
+{
+    return nom;
+}
+
+QString Enseignant::getPrenom()
+{
+    return prenom;
+}
+
+
+bool Enseignant::ajouter()
+{
+    QSqlQuery query;
+    query.prepare("INSERT INTO Enseignants (Nom, Prenom, Date_Naissance, Adresse, Email, Numero_Telephone, Date_Embauche, Matiere_Enseignee) "
+                  "VALUES (:nom, :prenom, :dateNaissance, :adresse, :email, :numeroTelephone, :dateEmbauche, :matiereEnseignee)");
+    query.bindValue(":nom", nom);
+    query.bindValue(":prenom", prenom);
+    query.bindValue(":dateNaissance", dateNaissance);
+    query.bindValue(":adresse", adresse);
+    query.bindValue(":email", email);
+    query.bindValue(":numeroTelephone", numeroTelephone);
+    query.bindValue(":dateEmbauche", dateEmbauche);
+    query.bindValue(":matiereEnseignee", matiereEnseignee);
+
+    if (query.exec()) {
+        return true;
+    } else {
+        qDebug() << "Error in ajouter(): " << query.lastError().text();
+        return false;
+    }
+}
+
+bool Enseignant::modifier()
+{
+    QSqlQuery query;
+    query.prepare("UPDATE Enseignants SET Nom = :nom, Prenom = :prenom, Date_Naissance = :dateNaissance, "
+                  "Adresse = :adresse, Email = :email, Numero_Telephone = :numeroTelephone, Date_Embauche = :dateEmbauche, "
+                  "Matiere_Enseignee = :matiereEnseignee WHERE ID_Enseignant = :enseignantId");
+    query.bindValue(":nom", nom);
+    query.bindValue(":prenom", prenom);
+    query.bindValue(":dateNaissance", dateNaissance);
+    query.bindValue(":adresse", adresse);
+    query.bindValue(":email", email);
+    query.bindValue(":numeroTelephone", numeroTelephone);
+    query.bindValue(":dateEmbauche", dateEmbauche);
+    query.bindValue(":matiereEnseignee", matiereEnseignee);
+    query.bindValue(":enseignantId", enseignantId);
+
+    if (query.exec()) {
+        return true;
+    } else {
+        qDebug() << "Error in modifier(): " << query.lastError().text();
+        return false;
+    }
+}
+
+bool Enseignant::supprimer()
+{
+    QSqlQuery query;
+    query.prepare("DELETE FROM Enseignants WHERE ID_Enseignant = :enseignantId");
+    query.bindValue(":enseignantId", enseignantId);
+
+    if (query.exec()) {
+        return true;
+    } else {
+        qDebug() << "Error in supprimer(): " << query.lastError().text();
+        return false;
+    }
+}
+
+
+QSqlQueryModel* Enseignant::rechercher(QString nom, QString prenom, QString matiereEnseignee)
+{
+    QSqlQueryModel* model = new QSqlQueryModel();
+    QSqlQuery query;
+
+    query.prepare("SELECT * FROM Enseignants WHERE Nom LIKE :nom AND Prenom LIKE :prenom AND Matiere_Enseignee LIKE :matiere");
+    query.bindValue(":nom", "%" + nom + "%");
+    query.bindValue(":prenom", "%" + prenom + "%");
+    query.bindValue(":matiere", "%" + matiereEnseignee + "%");
+
+    if (query.exec()) {
+        model->setQuery(query);
+    } else {
+        qDebug() << "Error in rechercher(): " << query.lastError().text();
+    }
+
+    return model;
+}
+
+QSqlQueryModel* Enseignant::trierParNomDateEmbaucheMatiere()
+{
+    QSqlQueryModel* model = new QSqlQueryModel();
+    model->setQuery("SELECT * FROM Enseignants ORDER BY Nom ASC, Date_Embauche ASC, Matiere_Enseignee ASC");
+    return model;
+}
